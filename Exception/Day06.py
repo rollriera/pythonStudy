f40e0722dc340e2d1fae5ed9ad15eb5525f10517
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-01 21:50:48 +0900

    예외 처리, 사용자 정의 예외 처리

--- Exception/Day06.py
+++ Exception/Day06.py
@@ -1,70 +1,74 @@
-# # 예외 처리
-# try :
-#     print("나누기 전용 계산기 입니다.")
-#     nums = []
-#     nums.append(int(input("첫 번째 숫자를 입력하세요 :")))
-#     nums.append(int(input("두 번째 숫자를 입력하세요 :")))
-#     #nums.append(int(nums[0] / nums[1]))
-#     print(f"{nums[0]} / {nums[1]} = {int(nums[2])}")
-# except ValueError :
-#     print("에러 ! 잘못된 값을 입력하였습니다.")    
-# except ZeroDivisionError as arr : # 0을 나눴을때의 에러
-#     print(arr) # 에러 문장 그대로 출력   
-# except Exception as err: # 위 에러 외의 다른 에러를 전부 잡아줌
-#     print("알 수 없는 에러가 발생하였습니다.")
-#     print(err) #에러 로그 확인
+# 예외 처리
+try :
+    print("나누기 전용 계산기 입니다.")
+    nums = []
+    nums.append(int(input("첫 번째 숫자를 입력하세요 :")))
+    nums.append(int(input("두 번째 숫자를 입력하세요 :")))
+    #print(f"{nums[0]} / {nums[1]} = {int(nums[2])}")
+    nums.append(int(nums[0] / nums[1]))
+    print(f"{nums[0]} / {nums[1]} = {int(nums[0] / nums[1])}")
+except ValueError  as arr:
+    print("에러 ! 잘못된 값을 입력하였습니다.")    
+    print(arr)
+except ZeroDivisionError as arr : # 0을 나눴을때의 에러
+    print(arr) # 에러 문장 그대로 출력   
+except Exception as err: # 위 에러 외의 다른 에러를 전부 잡아줌
+    print("알 수 없는 에러가 발생하였습니다.")
+    print(err) #에러 로그 확인
+finally :
+    print("계산기를 이용해 주셔서 감사합니다.")    
 
-# # 에러 발생 시키기
-# try :
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")       
-#     num1 = int(input("첫 번째 숫자를 입력하세요 :"))
-#     num2 = int(input("두 번째 숫자를 입력하세요."))
-#     if num1 >= 10 or num2 >= 10 :
-#         raise ValueError # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
-#     print(f"{num1} / {num2} = {int(num1 / num2)}")
-# except ValueError :
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")    
+# 에러 발생 시키기
+try :
+    print("한 자리 숫자 나누기 전용 계산기입니다.")       
+    num1 = int(input("첫 번째 숫자를 입력하세요 :"))
+    num2 = int(input("두 번째 숫자를 입력하세요."))
+    if num1 >= 10 or num2 >= 10 :
+        raise ValueError # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
+    print(f"{num1} / {num2} = {int(num1 / num2)}")
+except ValueError :
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")    
 
-# # 사용자 정의 예외 처리  
+# 사용자 정의 예외 처리  
 
-# class BigNumberError(Exception) :
-#     def __init__(self, msg) :
-#         self.msg = msg
+class BigNumberError(Exception) :
+    def __init__(self, msg) :
+        self.msg = msg
 
-#     def __str__(self) :
-#         return self.msg
+    def __str__(self) :
+        return self.msg
 
-# try :
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")       
-#     num1 = int(input("첫 번째 숫자를 입력하세요 :"))
-#     num2 = int(input("두 번째 숫자를 입력하세요."))
-#     if num1 >= 10 or num2 >= 10 :
+try :
+    print("한 자리 숫자 나누기 전용 계산기입니다.")       
+    num1 = int(input("첫 번째 숫자를 입력하세요 :"))
+    num2 = int(input("두 번째 숫자를 입력하세요 :"))
+    if num1 >= 10 or num2 >= 10 :
         
-#         raise BigNumberError(f"입력값 : {num1}, {num2}") # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
-#     print(f"{num1} / {num2} = {int(num1 / num2)}")
-# except ValueError :
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")   
-# except BigNumberError as arr:
-#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")    
-#     print(arr)
+        raise BigNumberError(f"입력값 : {num1}, {num2} 두 자리 숫자는 입력할 수 없습니다.") # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
+    print(f"{num1} / {num2} = {int(num1 / num2)}")
+except ValueError :
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")   
+except BigNumberError as arr:
+    print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")    
+    print(arr)
 
-# # finally
+# finally
 
-# try :
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")       
-#     num1 = int(input("첫 번째 숫자를 입력하세요 :"))
-#     num2 = int(input("두 번째 숫자를 입력하세요."))
-#     if num1 >= 10 or num2 >= 10 :
+try :
+    print("한 자리 숫자 나누기 전용 계산기입니다.")       
+    num1 = int(input("첫 번째 숫자를 입력하세요 :"))
+    num2 = int(input("두 번째 숫자를 입력하세요."))
+    if num1 >= 10 or num2 >= 10 :
         
-#         raise BigNumberError(f"입력값 : {num1}, {num2}") # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
-#     print(f"{num1} / {num2} = {int(num1 / num2)}")
-# except ValueError :
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")   
-# except BigNumberError as arr:
-#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")    
-#     print(arr)
-# finally :
-#     print("계산기를 이용해 주셔서 감사합니다.")
+        raise BigNumberError(f"입력값 : {num1}, {num2}") # 위 if 조건에 맞게되면 raise를 통해서 특정 해러를 발생시킴
+    print(f"{num1} / {num2} = {int(num1 / num2)}")
+except ValueError :
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")   
+except BigNumberError as arr:
+    print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")    
+    print(arr)
+finally :
+    print("계산기를 이용해 주셔서 감사합니다.")
 
 # 실습
 # 동네에 항상 대기 손님이 있는 맛있는 치킨집이 있습니다.
@@ -76,8 +80,6 @@
 # 조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
 #         치킨 소진 시 사용자 정의 에러[SoldOutError]를 발생시키고 프로그램 종료
 #         출력 메시지 : "재고가 소진되어 더이상 주문을 받지 않습니다.#
-
-print(type(-1))
 
 class SoldOutError(Exception) :
     def __init__(self) :
